1d3dffcca35028ddc0c46affc904f703c9972b4b
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-11 18:52:31 +0530

    Updated index.js
    Completed Exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const info = `Phonebook has info for ${persons.length} people\n\n${new Date()}`
+    response.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
