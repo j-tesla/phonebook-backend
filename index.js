fb2c87362ccb7d75361ea09280fdbf7eba6b9264
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-11 19:27:11 +0530

    Updated index.js
    Completed Exercise 3.3

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/info', (request, response) => {
     response.send(info)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    let id
+    id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person)
+        response.json(person)
+    else
+        response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
