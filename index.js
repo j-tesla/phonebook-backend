923fc1d969fc28324a07783667d3b13e50e3f6d2
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-14 11:13:37 +0530

    Connected to mongoDB
    Completed Exercise 3.15 - 3.18

--- index.js
+++ index.js
@@ -19,54 +19,27 @@ morgan.token('body', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: '39-23-6423122'
-    }
-]
 
-// const generateId = () => {
-//     return Math.round(Math.random() * 1000000000000000)
-// }
-
-app.get('/info', (request, response) => {
-    const info = `Phonebook has info for ${persons.length} people\n\n${new Date()}`
-    response.send(info)
+app.get('/info', (req, res, next) => {
+    Person.find({})
+        .then(people => {
+            res.send(`Phonebook has info for ${people.map(person => person.toJSON()).length} people\n\n${new Date()}`)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
+app.get('/api/persons', (req, res, next) => {
+    Person.find({})
+        .then(people => {
             res.json(people.map(person => person.toJSON()))
-        }
-    )
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (body.name && body.number) {
-        // if (persons.map(p => p.name).includes(body.name)) {
-        //     return res.status(400).json({
-        //         error: 'name must be unique'
-        //     })
-        // }
         const person = new Person({
             name: body.name,
             number: body.number
@@ -75,12 +48,7 @@ app.post('/api/persons', (req, res) => {
             .then(savedPerson => {
                 return res.json(savedPerson.toJSON())
             })
-            .catch(error => {
-                console.log('failed to save new entry to mongoDB', error.message)
-                return res.status(502).json({
-                    error: 'failed to save new entry to database'
-                })
-            })
+            .catch(error => next(error))
 
     } else {
         return res.status(400).json({
@@ -89,22 +57,67 @@ app.post('/api/persons', (req, res) => {
     }
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    let id
-    id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person)
-        res.json(person)
-    else
-        res.status(404).end()
+app.get('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log(req.params.id)
+    Person.findById(id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON())
+            } else
+                return res.status(404).send({error: 'requested resource not found'})
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    if (body.name && body.number) {
+        const updatedPerson = {
+            name: body.name,
+            number: body.number
+        }
+        Person.findByIdAndUpdate(req.params.id, updatedPerson, {new: true})
+            .then(person => {
+                if (person)
+                    res.json(person.toJSON())
+                else
+                    return res.status(404).send({error: 'requested resource not found'})
+            })
+            .catch(error => next(error))
+    } else {
+        return res.status(400).json({
+            error: 'both name and number fields are necessary'
+        })
+    }
+})
+
+// TODO middleware for error handling and unidentified requests
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).send({error: 'malformatted id'})
+    }
+
+    console.log(error)
+    return res.status(500).send({error: `${error.name}: ${error.message}`})
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
